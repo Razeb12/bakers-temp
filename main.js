5e6510f9fde48f38615e95fb060d0c72e776c492
Razeb Enyi <enyirazeb@gmail.com>
2020-07-10 07:52:08 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+$(function () {
+    $("#solfas-testimonial").owlCarousel({
+        items: 3,
+        autoPlay: true,
+        smartSpeed: 400,
+        look: true,
+        autoPlayHoverPause: true,
+    });
+});
+$(document).ready(function () {
+    // Check if element is scrolled into view
+    function isScrolledIntoView(elem) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+
+        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }
+    // If element is scrolled into view, fade it in
+    $(window).scroll(function () {
+        $('.scroll-animations .animated').each(function () {
+            if (isScrolledIntoView(this) === true) {
+                $(this).addClass('zoomIn');
+            }
+        });
+    });
+});
